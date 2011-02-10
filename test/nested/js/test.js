b52189a4101818774cca57721d89977222ad28ec
bnoguchi <brian.noguchi@gmail.com>
2011-02-09 22:47:39 -0800

    Added tests back in.

---
+++ test/nested/js/test.js
@@ -0,0 +1,51 @@
+var rel = require('./rel')
+  , nested = require('./nested/nest')
+  , aboveNested = require('./nested/aboveNested')
+  , elevator = require('./level1')
+  , hell = require('../../hell');
+
+window.module('require relatives');
+asyncTest('relative sibling requires', function () {
+  equals(rel(), 'Hello World!');
+  start();
+});
+asyncTest('relative nested requires', function () {
+  equals(nested(), "I'm a descendant");
+  start();
+});
+asyncTest('relative above nested requires', function () {
+  equals(aboveNested(), "I'm an ancestor of the descendant");
+  start();
+});
+asyncTest('elevator requires', function () {
+  equals(elevator(), "level 1 => level 3 => level 2");
+  start();
+});
+asyncTest('level before base dir require', function () {
+  equals(hell(), "PYRO");
+  start();
+});
+
+window.module('require npm');
+asyncTest('simple npm modules', function () {
+  var _ = require('underscore');
+  equals(10, _.reduce([1, 2, 3, 4], function (sum, num) {
+    sum += num;
+    return sum;
+  }));
+  start();
+});
+asyncTest('npm submodules', function () {
+  var Set = require('data-structures-js/set')
+    , s = new Set(['look', 'ma', 'no', 'hands']);
+  s.add('look');
+  ok(s.contains('ma'));
+  start();
+});
+asyncTest('npm modules that use relative modules', function () {
+  // The following file requires ./entities;
+  // See https://github.com/chriso/node-validator/blob/master/lib/xss.js
+  var clean = require('validator/xss').clean;
+  equals('function', typeof clean);
+  start();
+});
---
+++ test/nested/server.js
@@ -0,0 +1,13 @@
+require.paths.unshift('..');
+var path = require('path');
+require.paths.unshift(path.join(__dirname, '..', '..'));
+var connect = require('connect');
+var app = connect.createServer();
+var exposeRequire = require('browser-require');
+app.use(exposeRequire({
+  base: __dirname
+}));
+app.use(connect.staticProvider(__dirname));
+app.listen(1234);
+console.log("Server running at http://127.0.0.1:1234");
+process.title = "brtest";
