c692c222b646bb991328d8f8aab76509eea8fd27
bnoguchi <brian.noguchi@gmail.com>
2011-02-09 19:40:53 -0800

    Re-factored code so all of the npm work is done in NpmModule.prototype methods with results sent to callback params.

--- browser-require.js
+++ browser-require.js
@@ -30,6 +30,98 @@ module.exports = function (opts) {
     return deps;
   }
 
+  function NpmModule (url) {
+    var modulePath = url.replace(NpmModule.npmFlag, '').replace(/\.js$/, '')
+      , moduleChain = modulePath.split('/')
+      , pkgName = this.pkgName = moduleChain[0]
+      , relChain = this.relChain = moduleChain.slice(1);
+    this.isSubmodule = !!relChain.length;
+    var nv = pkgName.split('@')
+      , n = this.name = nv[0]
+      , v = this.ver = nv[1] || 'active';
+    this.dir = path.join(npm.dir, n, v, 'package');
+  }
+  NpmModule.npmFlag = /^\/NPM\//;
+
+  NpmModule.prototype = {
+    pkgJson: function (fn) {
+      if (this._pkg) return fn(null, this._pkg);
+      if (this._pkgerr) return fn(this._pkgerr);
+      var self = this;
+      fs.readFile(this.dir + '/package.json', 'utf8', function (err, body) {
+        if (err) {
+          self._pkgerr = err;
+          console.error("package.json missing for " + self.pkgName);
+          return fn(err);
+        }
+        var pkg = self._pkg = JSON.parse(body);
+        fn(null, pkg);
+      });
+    },
+    mainSrc: function (fn) {
+      if (this._mainSrc) return fn(null, this._mainSrc);
+      if (this._mainSrcErr) return fn(this._mainSrcErr);
+      var self = this;
+      this.pkgJson( function (err, pkg) {
+        if (err) return fn(self._mainSrcErr = err);
+        if (pkg.main) {
+          fs.stat(self.dir + '/' + pkg.main, function (err, stat) {
+            if (err) {
+              console.error(err);
+              fn(self._mainSrcErr = err);
+            } else if (stat.isDirectory()) {
+              self._mainSrc = fs.readFileSync(self.dir + '/' + pkg.main + '/index.js', 'utf8');
+              fn(null, self._mainSrc);
+            } else {
+              self._mainSrc = fs.readFileSync(self.dir + '/' + pkg.main, 'utf8');
+              fn(null, self._mainSrc);
+            }
+          });
+        } else {
+          fn(self.mainSrcErr = "Missing main in package.json for " + self.pkgName);
+        }
+      });
+    },
+    relSrc: function (fn) {
+      if (this._relSrc) return fn(null, this._relSrc);
+      if (this._relSrcErr) return fn(this._relSrcErr);
+      var self = this;
+      this.pkgJson( function (err, pkg) {
+        if (err) return fn(self._relSrcErr = err);
+        var directories = pkg.directory || pkg.directories
+          , lib = directories.lib
+          , dest;
+        if (lib) {
+          dest = path.join(self.dir, lib, self.relChain.join('/') + '.js');
+          if (path.existsSync(dest)) {
+            fn(null, fs.readFileSync(dest, 'utf8'));
+          } else {
+            throw new Error("Unimplemented");
+          }
+        } else {
+          self._relSrcErr = 'Missing ' + self.relChain.join('/') + ' in ' + self.pkgName + ' package';
+          fn(self._relSrcErr);
+        }
+      });
+    },
+    src: function (fn) {
+      if (this._src) return fn(null, this._src);
+      if (this._srcerr) return fn(this._srcerr);
+      var self = this;
+      if (this.isSubmodule) { // Handle e.g., require("npm-module/sub-module")
+        this.relSrc( function (err, relSrc) {
+          if (err) return fn(self._srcerr = err);
+          fn(null, self._src = relSrc);
+        });
+      } else { // Handle e.g., require("npm-module")
+        this.mainSrc( function (err, mainSrc) {
+          if (err) return fn(self._srcerr = err);
+          fn(null, self._src = mainSrc);
+        });
+      }
+    }
+  };
+
   // Wrap npm as promise $npm, for manageable async middleware
   var $npm = (function () {
     var isLoaded = false
@@ -42,19 +134,15 @@ module.exports = function (opts) {
       }
     });
 
-    var isNpmModule = function (name, fn) {
-      if (isLoaded) return _isNpmModule(name, fn);
+    var isNpmModule = function (module, fn) {
+      if (isLoaded) return _isNpmModule(module, fn);
       callbacks.push([_isNpmModule, arguments]);
     };
 
-    var _isNpmModule = function (name, fn) {
-      var nv = name.split('@')
-        , n = nv[0]
-        , v = nv[1] || 'active'
-        , dir = path.join(npm.dir, n, v, 'package');
-      fs.stat(dir, function (err, stat) {
+    var _isNpmModule = function (module, fn) {
+      fs.stat(module.dir, function (err, stat) {
         if (err || !stat.isDirectory()) {
-          console.error(name + " is not installed via npm.");
+          console.error(module.name + " is not installed via npm.");
           fn(err, false);
         } else {
           fn(null, true);
@@ -62,47 +150,13 @@ module.exports = function (opts) {
       });
     };
 
-    var loadNpmModule = function (name, relChain, fn) {
-      if (isLoaded) return _loadNpmModule(name, relChain, fn);
+    var loadNpmModule = function (module, fn) {
+      if (isLoaded) return _loadNpmModule(module, fn);
       callbacks.push([_loadNpmModule, arguments]);
     };
 
-    var _loadNpmModule = function (name, relChain, fn) {
-      var nv = name.split('@')
-        , n = nv[0]
-        , v = nv[1] || 'active'
-        , dir = path.join(npm.dir, n, v, 'package');
-      fs.readFile(dir + '/package.json', 'utf8', function (err, body) {
-        if (err) { console.error("package.json missing for " + name); return fn(err); }
-        var pkg = JSON.parse(body);
-        if (!relChain.length) { // Handle require("npm-module")
-          if (pkg.main) {
-            fs.stat(dir + '/' + pkg.main, function (err, stat) {
-              if (err) {
-                console.error(err);
-                fn(err);
-              } else if (stat.isDirectory()) {
-                fn(null, fs.readFileSync(dir + '/' + pkg.main + '/index.js', 'utf8'));
-              } else {
-                fn(null, fs.readFileSync(dir + '/' + pkg.main, 'utf8'));
-              }
-            });
-          }
-          else fn("Missing main in package.json for " + name);
-        } else { // Handle e.g., require("npm-module/sub-module")
-          var directories = pkg.directory || pkg.directories
-            , lib = directories.lib
-            , dest;
-          if (lib) {
-            dest = path.join(dir, lib, relChain.join('/') + '.js');
-            if (path.existsSync(dest)) {
-              fn(null, fs.readFileSync(dest));
-            } else {
-              throw new Error("Unimplemented");
-            }
-          } else fn("Missing " + relChain.join('/') + " in " + name + " package");
-        }
-      });
+    var _loadNpmModule = function (module, fn) {
+      module.src(fn);
     };
 
     return {
@@ -116,8 +170,7 @@ module.exports = function (opts) {
     var src
       , url = req.url
       , body
-      , filepath
-      , npmFlag = /^\/NPM\//;
+      , filepath;
     if (src = cache[url]) {
       res.writeHead(200, {'Content-Type': 'text/javascript'});
       res.end(src);
@@ -128,14 +181,11 @@ module.exports = function (opts) {
 
         res.writeHead(200, {'Content-Type': 'text/javascript'});
         res.end(src);
-      } else if (npmFlag.test(url)) {
-        var modulePath = url.replace(npmFlag, '').replace(/\.js$/, '')
-          , moduleChain = modulePath.split('/')
-          , pkgName = moduleChain[0]
-          , relChain = moduleChain.slice(1);
-        $npm.isNpmModule(pkgName, function (err, isNpm) {
+      } else if (NpmModule.npmFlag.test(url)) { // Handle npm modules
+        var npmModule = new NpmModule(url);
+        $npm.isNpmModule(npmModule, function (err, isNpm) {
           if (isNpm) {
-            $npm.loadNpmModule(pkgName, relChain, function (err, body) {
+            $npm.loadNpmModule(npmModule, function (err, body) {
               var src = 
                 cache[url] = fillinTemplate(url, body, depsFor(body));
               res.writeHead(200, {'Content-Type': 'text/javascript'});
@@ -147,7 +197,7 @@ module.exports = function (opts) {
             res.end();
           }
         });
-      } else {
+      } else { // Handle local, relative modules
         filepath = path.join(baseDir, url);
         if (path.existsSync(filepath)) {
           body = fs.readFileSync(filepath, 'utf8');
