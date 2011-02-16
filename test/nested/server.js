fb780f2c24052728c6b5c14910b7119af24b91f1
bnoguchi <brian.noguchi@gmail.com>
2011-02-15 18:46:20 -0800

    Finally got old tests passing again.

--- test/nested/server.js
+++ test/nested/server.js
@@ -1,13 +1,14 @@
-require.paths.unshift('../lib');
+require.paths.unshift('../../lib');
 var path = require('path');
 require.paths.unshift(path.join(__dirname, '..', '..'));
 var connect = require('connect');
-var app = connect.createServer();
 var exposeRequire = require('browser-require');
-app.use(exposeRequire({
-  base: __dirname
-}));
-app.use(connect.staticProvider(__dirname));
+var app = connect.createServer(
+  exposeRequire({
+    base: __dirname
+  }),
+  connect.staticProvider(__dirname)
+);
 app.listen(1234);
 console.log("Server running at http://127.0.0.1:1234");
 process.title = "brtest";
